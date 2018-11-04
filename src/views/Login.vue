dac52b5626756a8496ab3fc350330b18fb45d505
nasir522004 <nasir522004@gmail.com>
2018-11-04 20:36:12 +0700

    [ADD] เพิ่ม หน้า login และ อัพ logo seal-learning

---
+++ src/views/Login.vue
@@ -0,0 +1,79 @@
+<template>
+<v-app id = "background">
+  <v-content>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-card-text>
+                <img src="https://www.sit.kmutt.ac.th/en/wp-content/uploads/2018/05/logo-flat-blk.png"/>  
+                <v-form>
+                  <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
+                  <v-text-field prepend-icon="lock" name="password" label="Password" id="password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+                <v-btn color="primary">Login</v-btn>
+                <hr/>
+                <v-btn @click="login_facebook()">Facebook</v-btn>
+                <br/><br/>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+  </v-content>
+</v-app>  
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "login",
+  data() {
+    return {
+      profile: {},
+      ready: false,
+      authorized: false
+    };
+  },
+  methods: {
+    async login_facebook() {
+      await FB.getLoginStatus(function(response) {
+        console.log(response);
+        const api = axios.create({
+          headers: {
+            access: response
+          }
+        });
+        const data = api
+          .post("", {
+
+          })
+          .then(function(response) {
+            
+          })
+          .catch(function(response) {
+            alert("มีบางอย่าง error !!!");
+          });
+      });
+    }
+  },
+  async mounted() {
+    window.fbAsyncInit = () => {
+      FB.init({
+        appId: "1910900992326991",
+        cookie: true,
+        xfbml: true,
+        version: "v3.2"
+      });
+    };
+  }
+};
+</script>
+
+<style>
+#background {
+  background-image: url("https://scontent.fbkk6-2.fna.fbcdn.net/v/t1.0-9/23844857_10155288832968789_8091596763212753375_n.jpg?_nc_cat=106&_nc_eui2=AeHyGuSX8hHWQacYDXKfUsobwJMBvCSdItX1D8Ts_Y42CbXFJhQaYSTIs59HjOh5svKrP9hr-FauQw2OPCI288gC7Op93DpB1P6Np98wAVBMZQ&_nc_ht=scontent.fbkk6-2.fna&oh=0f563d4bc2aa5a3e965282d4dda9bc96&oe=5C781663");
+  background-size: 100%;
+}
+</style>
