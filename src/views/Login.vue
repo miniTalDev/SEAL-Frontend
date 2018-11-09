2fc74c4573329f2232594c5834c83ad1f6d8d4f4
nasir522004 <nasir522004@gmail.com>
2018-11-09 22:53:18 +0700

    [EDIT] master : update login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,27 +1,26 @@
 <template>
-<v-app id = "background">
-  <v-content>
-    <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-12">
-              <v-card-text>
-                <img src="https://www.sit.kmutt.ac.th/en/wp-content/uploads/2018/05/logo-flat-blk.png"/>
-                <v-form>
-                  <v-text-field v-model="username" prepend-icon="person" name="login" label="Login" type="text" ></v-text-field>
-                  <v-text-field v-model="password" prepend-icon="lock" name="password" label="Password" id="password" type="password"></v-text-field>
-                </v-form>
-              </v-card-text>
-                <v-btn color="rgb(163,190,140)" @click="loginAuthen()">Login</v-btn>
-                <hr/>
-                <v-btn @click="login_facebook()" color="rgba(59, 89, 152)" style="color:white">Facebook</v-btn>
-                <br/><br/>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-  </v-content>
-</v-app>
+  <v-app>
+    <v-container fluid id="background"></v-container>
+    <v-container fluid fill-height class="login">
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="login-block">
+            <v-card-text>
+              <img class="logo-img" src="https://www.sit.kmutt.ac.th/en/wp-content/uploads/2018/05/logo-flat-blk.png"/>
+              <v-form>
+                <v-text-field v-model="username" prepend-icon="person" name="login" label="Login" type="text" ></v-text-field>
+                <v-text-field v-model="password" prepend-icon="lock" name="password" label="Password" id="password" type="password"></v-text-field>
+              </v-form>
+            </v-card-text>
+              <v-btn color="rgb(163,190,140)" @click="loginAuthen()">Login</v-btn>
+              <hr/>
+              <v-btn @click="login_facebook()" color="rgba(59, 89, 152)" style="color:white">Facebook</v-btn>
+              <br/><br/>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>  
+  </v-app>
 </template>
 
 <script>
@@ -93,8 +92,32 @@ export default {
 </script>
 
 <style>
-#background {
-  background-image: url("https://scontent.fbkk6-2.fna.fbcdn.net/v/t1.0-9/23844857_10155288832968789_8091596763212753375_n.jpg?_nc_cat=106&_nc_eui2=AeHyGuSX8hHWQacYDXKfUsobwJMBvCSdItX1D8Ts_Y42CbXFJhQaYSTIs59HjOh5svKrP9hr-FauQw2OPCI288gC7Op93DpB1P6Np98wAVBMZQ&_nc_ht=scontent.fbkk6-2.fna&oh=0f563d4bc2aa5a3e965282d4dda9bc96&oe=5C781663");
-  background-size: 100%;
-}
+  .v-navigation-drawer, .v-toolbar, .v-footer {
+    display: none;
+  }
+</style>
+
+<style scoped>
+  #background {
+    background-image: url("https://scontent.fbkk6-2.fna.fbcdn.net/v/t1.0-9/23844857_10155288832968789_8091596763212753375_n.jpg?_nc_cat=106&_nc_eui2=AeHyGuSX8hHWQacYDXKfUsobwJMBvCSdItX1D8Ts_Y42CbXFJhQaYSTIs59HjOh5svKrP9hr-FauQw2OPCI288gC7Op93DpB1P6Np98wAVBMZQ&_nc_ht=scontent.fbkk6-2.fna&oh=0f563d4bc2aa5a3e965282d4dda9bc96&oe=5C781663");
+    background-size: 100%;
+    filter: blur(5px);
+  }
+
+  .login {
+    position: absolute;
+  }
+
+  .login-block {
+    border-radius: 10px;
+  }
+
+  .logo-img {
+    width: 120px;
+    height: auto;
+  }
+
+  .v-btn {
+    width: 80%;
+  }
 </style>
