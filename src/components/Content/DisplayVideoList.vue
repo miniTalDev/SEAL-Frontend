281c67cc98b3019fe839038a54b15422155b946b
nasir522004 <nasir522004@gmail.com>
2018-11-24 15:07:29 +0700

    [UPDATE] add header Authorization in axios & format code

--- src/components/Content/DisplayVideoList.vue
+++ src/components/Content/DisplayVideoList.vue
@@ -1,29 +1,34 @@
 <template>
 <v-content>
-    <v-container fluid>
-          <v-container grid-list-md >
-            <v-layout row wrap align-end flexbox>
-                <v-flex v-for="video in videoDetails" :key="video.video_id" xs12 sm3>
-                    <router-link :to="`/player/${video.video_id}`" @click="testLoad()">
-                      <video-card :imgURL="video.video_thumbnail" :videoID="video.video_id" :videoDate="video.video_date"
-                      :teacherName="video.teacher.teacher_name" :subjectName="video.video_name"  />
-                    </router-link>
-                </v-flex>
-            </v-layout>
-          </v-container>
-      </v-container>
+  <v-container fluid>
+    <v-container grid-list-md >
+      <v-layout row wrap align-end flexbox>
+        <v-flex v-for="video in videoDetails" :key="video.video_id" xs12 sm3>
+          <router-link :to="`/player/${video.video_id}`" @click="testLoad()">
+            <video-card
+              :imgURL="video.video_thumbnail"
+              :videoID="video.video_id"
+              :videoDate="video.video_date"
+              :teacherName="video.teacher.teacher_name"
+              :subjectName="video.video_name"
+            />
+          </router-link>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-container>
 
   <v-dialog v-model="dialog" hide-overlay persistent width="300">
     <v-card color="black" dark>
-        <v-card-text>
-          Loading...
-          <v-progress-linear
-            indeterminate
-            color="white"
-            class="mb-0"
-          ></v-progress-linear>
-        </v-card-text>
-      </v-card>
+      <v-card-text>
+        Loading...
+        <v-progress-linear
+          indeterminate
+          color="white"
+          class="mb-0"
+        ></v-progress-linear>
+      </v-card-text>
+    </v-card>
   </v-dialog>
 
 </v-content>
@@ -57,9 +62,14 @@ export default {
   },
   methods: {
     loadAllVideoCard: async function () {
+      let jwtTokenLocalStorage = localStorage.getItem('jwtToken')
       let videoDetails = await axios.get(
-        process.env.VUE_APP_VIDEO_SERVICE_URL +
-          `/subject/${this.subjectID}/videos`
+        `${process.env.VUE_APP_VIDEO_SERVICE_URL}/subject/${this.subjectID}/videos`,
+        {
+          headers: {
+            Authorization: `Bearer ${jwtTokenLocalStorage}`
+          }
+        }
       )
       videoDetails = videoDetails.data
       this.videoDetails = videoDetails
