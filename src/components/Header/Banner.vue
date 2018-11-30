7a86814380e190ea1d1a842beba60334c79fac6b
nasir522004 <nasir522004@gmail.com>
2018-11-30 20:43:10 +0700

    [EDIT] แก้รูป หนิหน่อย

--- src/components/Header/Banner.vue
+++ src/components/Header/Banner.vue
@@ -1,6 +1,6 @@
 <template>
 <v-content>
-    <v-carousel hide-controls width="5">
+    <v-carousel hide-controls width="5" delimiter-icon="school">
     <v-carousel-item
       v-for="(item,i) in items"
       :key="i"
@@ -28,7 +28,7 @@ export default {
           src: 'https://scontent.fbkk12-2.fna.fbcdn.net/v/t1.0-9/44190230_1600685520036865_4372677590985474048_o.jpg?_nc_cat=104&_nc_ht=scontent.fbkk12-2.fna&oh=c2feebf32444c6547f8d7038f09e7bcc&oe=5C4AB8F8'
         },
         {
-          src: 'https://www.sit.kmutt.ac.th/wp-content/uploads/2018/02/banner-onfront02_03.jpg'
+          src: 'https://scontent.fbkk12-2.fna.fbcdn.net/v/t1.0-9/43534328_2303477516338898_1061947946884399104_o.jpg?_nc_cat=104&_nc_eui2=AeF_ZLIdaNS-U2OF9O789NIVuYdc8iYxMnTuKK4FIjv7I48-iWUZtJ8CpK7SNtHh0ds6ztF0RYa0KXnDC1MDle4aKK3vOJ5Dp0p-1oZq1ukvAw&_nc_ht=scontent.fbkk12-2.fna&oh=7441c7aa8fa626e2edea4b0637103185&oe=5CB0F203'
         },
         {
           src: 'https://www.bhubeth.com/wp-content/uploads/2013/04/sit-kmutt.jpg'
